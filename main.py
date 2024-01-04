031d69f1c0ae3011deebccbaf7432784d6fbacf8
HAWONA RODGERS WISDOM <130440506+rodgerswisdom@users.noreply.github.com>
2024-01-04 18:19:02 +0300

    function to read excel file

--- main.py
+++ main.py
@@ -4,11 +4,19 @@
     - Make a request to openai
     - save response in excel
 """
+#import important modules
 from openai import OpenAI
 import os.path
 import requests
 import pandas as pd
 from api_keys import API_KEY
+
+#function to read excel file
+def read_excel(file_path):
+    df = pd.read_excel(file_path)
+    return df
+
+
 
 
 
