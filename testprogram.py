351c44f5e2c8b8559fa2bd0e72899d45309326c5
HAWONA RODGERS WISDOM <130440506+rodgerswisdom@users.noreply.github.com>
2024-01-05 16:19:50 +0300

    fix test program

--- testprogram.py
+++ testprogram.py
@@ -3,14 +3,14 @@ from openai import OpenAI
 from api_keys import API_KEY
 import pandas as pd
 
-user_input = "Interior design AI generator"
+
 
 client = OpenAI(api_key=API_KEY)
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": f"Expand the one-liner description below to 55 words using 2 sentences:{user_input}"}
+    {"role": "user", "content": "Hello"}
   ]
 )
 
